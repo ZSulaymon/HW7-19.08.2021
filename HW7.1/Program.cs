60358c315073a539b66f59ba91c9d896995814f9
SulaymonZafarov <sulaymonwork@gmail.com>
2021-08-20 13:59:52 +0500

    Add project files.

---
+++ HW7.1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace HW7._1
+{
+    class Rectangle
+    {
+        public double side1, side2;
+
+        //V1
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;                         //инициализация значение 
+            this.side2 = side2;
+        }
+        //V2
+        //public Rectangle()
+        //{                                                   //инициализация значение          
+        //}
+        public double AreaCalculator()                  //Метод вычисляющие площадь прямоугольника
+        {
+            return side1 * side2;
+            //return;
+        }
+        public double PerimeterCalculator()              //Метод вычисляющие Переметр прямоугольника
+        {
+            return 2 * AreaCalculator();
+        }
+        public double Area
+        {                                                    // получить площад
+            get { return AreaCalculator(); }
+        }
+        public double Perimeter
+        {                                               // получить перимтр
+            get { return PerimeterCalculator(); }
+        }
+    }
+
+
+    //new class--V3
+    //    class Rectangleе
+    //    { 
+    //        public double area, Perimeter;
+    //          public Rectangleе()
+    //            {
+    //            }
+    //}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                            //V1
+            Console.WriteLine("Введите площад прямоугольника:");
+            double.TryParse(Console.ReadLine(), out double side1);
+            Console.WriteLine("Введите перимтр прямоугольника:");
+            double.TryParse(Console.ReadLine(), out double side2);
+            var rectangle = new Rectangle(side1, side2);
+            Console.WriteLine("Площад: {0}, Прямоуголник: {1}", rectangle.Area, rectangle.Perimeter);
+
+
+                            //V2
+            //var rectangle = new Rectangle();
+            //Console.WriteLine("Введите площад прямоугольника:");
+            //rectangle.side1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите перимтр прямоугольника:");
+            //rectangle.side2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Площад: {0}, Прямоуголник: {1}", rectangle.Area, rectangle.Perimeter);
+
+                            //V3
+            //var rectanglee  = new Rectangleе();
+            //Console.WriteLine("Введите площад прямоугольника:");
+            //rectanglee.area = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите перимтр прямоугольника:");
+            //rectanglee.Perimeter = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Площад: {0}, Прямоуголник: {1}", rectanglee.area * rectanglee.Perimeter, (rectanglee.area * rectanglee.Perimeter)*2);
+        }
+    }
+}
---
+++ HW7.2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace HW7._2
+{
+    class Book
+    {
+        public string Name;
+
+        public void Show()
+        {
+            Console.WriteLine("Имя книги: {0} ", Name);
+            //Console.WriteLine($"Имя книги: {Name} ") ;
+        }
+    }
+
+    class Title
+    {
+        public string titleBook;
+
+        public void Show()
+        {
+            Console.WriteLine("Заголовок: {0}", titleBook);
+        }
+    }
+
+    class Author
+    {
+        public string AuthorName;
+
+        public void Show()
+        {
+            Console.WriteLine("Имя автора: {0}", AuthorName);
+        }
+    }
+
+    class Content
+    {
+        public string ContentBook;
+
+        public void Show()
+        {
+            Console.WriteLine("Содержание: {0} ", ContentBook);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book();
+            Console.WriteLine("Введите имя книги:");
+            book.Name = Console.ReadLine();
+
+            var title = new Title();
+            Console.WriteLine("Введите заголовок книги:");
+            title.titleBook = Console.ReadLine();
+
+            var author = new Author();
+            Console.WriteLine("Введите имя автора:");
+            author.AuthorName = Console.ReadLine();
+
+            var content = new Content();
+            Console.WriteLine("Введите содержание книги:");
+            content.ContentBook = Console.ReadLine();
+
+            book.Show();
+            title.Show();
+            author.Show();
+            content.Show();
+        }
+    }
+}
